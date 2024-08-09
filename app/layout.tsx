8104fcb383c973b9e9ea77d49d1db38ecc9633f9
jahzlariosa <devjahz@gmail.com>
2024-08-09 13:07:57 +0800

    Dark Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/app/components/Providers/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,10 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <Toaster />
-        {children}
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
+      <head />
+      <body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
